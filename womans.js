a16dde6ca84c1a5047aede54dbf406614463cd97
Gessica Oliveira <114042969+gessicaoliveira@users.noreply.github.com>
2023-04-26 22:05:31 -0300

    method post build and install uuid

--- womans.js
+++ womans.js
@@ -1,34 +1,54 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express'); //iniciando express
+const router = express.Router(); //config 1° parte rota
+const { v4:uuidv4 } = require('uuid');
 
-const app = express();
-const port = 3333;
+const app = express(); //iniciando app
+app.use(express.json()); //dados trafegando requisição format json
+const port = 3333; //criando a porta
 
+//lista incial de mulheres
 const womans = [
     {
-        nome: 'Gessica Oliveira',
+        name: 'Gessica Oliveira',
         github: 'https://github.com/gessicaoliveira',
         tinybio: 'Developer full stack jr',
     },
     {
-        nome: 'Conceição Evaristo',
+        name: 'Conceição Evaristo',
         image: 'https://i.postimg.cc/Z9XzNNMS/conceicao-evaristo.png',
         tinybio: 'Writer',
     },
     {
-        nome: 'Nina da Hora',
+        name: 'Nina da Hora',
         image: 'https://bit.ly/3FKpFaz',
         tinybio: 'Antiracist hacker',
     },
 ];
 
+//GET
 function showWomans(request, response) {
     response.send(womans);
 }
 
+//POST
+function createWoman(request, response) {
+    const newWoman = {
+        id: uuidv4(),
+        name: request.body.name,
+        image: request.body.image,
+        tinybio: request.body.tinybio
+    }
+
+    womans.push(newWoman);
+    response.json(womans);
+}
+
+//PORT
 function showPort() {
     console.log(`Servidor criado e rodando na porta ${port}`);
 }
 
-app.use(router.get('/womans', showWomans));
-app.listen(port, showPort);
+
+app.use(router.get('/womans', showWomans)); // confg rota GET /womans
+app.use(router.post('/womans', createWoman)); //config rota POST /womans
+app.listen(port, showPort); // servidor ouvindo porta
