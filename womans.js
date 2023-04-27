7a0c3da491c5516405b3c0f35bd4d573bb29e5fe
Gessica Oliveira <114042969+gessicaoliveira@users.noreply.github.com>
2023-04-26 22:47:57 -0300

    method patch build

--- womans.js
+++ womans.js
@@ -9,16 +9,19 @@ const port = 3333; //criando a porta
 //lista incial de mulheres
 const womans = [
     {
+        id: '1',
         name: 'Gessica Oliveira',
-        github: 'https://github.com/gessicaoliveira',
+        image: 'https://gerenciador.popload.com.br/wp-content/uploads/2018/01/180118_brodydalle2.jpg',
         tinybio: 'Developer full stack jr',
     },
     {
+        id: '2',
         name: 'Conceição Evaristo',
         image: 'https://i.postimg.cc/Z9XzNNMS/conceicao-evaristo.png',
         tinybio: 'Writer',
     },
     {
+        id: '3',
         name: 'Nina da Hora',
         image: 'https://bit.ly/3FKpFaz',
         tinybio: 'Antiracist hacker',
@@ -43,12 +46,36 @@ function createWoman(request, response) {
     response.json(womans);
 }
 
+//PATCH
+function correctWoman(request, response) {
+    function findWoman(woman) {
+        if (woman.id === request.params.id) {
+            return woman;
+        }
+    }
+
+    const womanFound = womans.find(findWoman);
+
+    if (request.body.name) {
+        womanFound.name = request.body.name;
+    }
+    if (request.body.image) {
+        womanFound.image = request.body.image;
+    }
+    if (request.body.tinybio) {
+        womanFound.tinybio = request.body.tinybio;
+    }
+
+    response.json(womans);
+
+}
+
 //PORT
 function showPort() {
     console.log(`Servidor criado e rodando na porta ${port}`);
 }
 
-
 app.use(router.get('/womans', showWomans)); // confg rota GET /womans
 app.use(router.post('/womans', createWoman)); //config rota POST /womans
+app.use(router.patch('/woman/:id', correctWoman)); //config PATCH /womans
 app.listen(port, showPort); // servidor ouvindo porta
