ee25f7c9e63704fafad642f397c1be6b334d2125
Gessica Oliveira <114042969+gessicaoliveira@users.noreply.github.com>
2023-04-28 11:17:10 -0300

    created model and update functions for the database

--- womans.js
+++ womans.js
@@ -1,80 +1,96 @@
 const express = require('express'); //iniciando express
 const router = express.Router(); //config 1° parte rota
-const { v4:uuidv4 } = require('uuid'); //biblioteca que gera id automaticamente
+
 const connectDatabase = require('./database');
 
 const connDatabase = require('./database'); //ligando ao arquivo banco de dados
 connectDatabase(); //chamando função que conecta o banco
 
+const Woman = require('.model'); //model criada
 const app = express(); //iniciando app
 app.use(express.json()); //dados trafegando requisição format json
 const port = 3333; //criando a porta
 
 //lista incial de mulheres
-const womans = [
-    {
-        id: '1',
-        name: 'Gessica Oliveira',
-        image: 'https://gerenciador.popload.com.br/wp-content/uploads/2018/01/180118_brodydalle2.jpg',
-        tinybio: 'Developer full stack jr',
-        quote: 'Nada resiste ao trabalho.'
-    },
-    {
-        id: '2',
-        name: 'Simara Conceição',
-        image: 'https://github.com/simaraconceicao.png',
-        tinybio: 'Developer and Instructor',
-        quote: ''
-    },
-    {
-        id: '3',
-        name: 'Nina da Hora',
-        image: 'https://bit.ly/3FKpFaz',
-        tinybio: 'Antiracist Hacker',
-        quote: ''
-    },
-];
+// const womans = [
+//     {
+//         id: '1',
+//         name: 'Gessica Oliveira',
+//         image: 'https://gerenciador.popload.com.br/wp-content/uploads/2018/01/180118_brodydalle2.jpg',
+//         tinybio: 'Developer full stack jr',
+//         quote: 'Nada resiste ao trabalho.'
+//     },
+//     {
+//         id: '2',
+//         name: 'Simara Conceição',
+//         image: 'https://github.com/simaraconceicao.png',
+//         tinybio: 'Developer and Instructor',
+//         quote: ''
+//     },
+//     {
+//         id: '3',
+//         name: 'Nina da Hora',
+//         image: 'https://bit.ly/3FKpFaz',
+//         tinybio: 'Antiracist Hacker',
+//         quote: ''
+//     },
+// ];
 
 //GET
-function showWomans(request, response) {
-    response.send(womans);
+async function showWomans(request, response) {
+    try {
+        const womansFromTheDatabase = await Woman.find()
+
+        response.json(womansFromTheDatabase);
+    } catch(erro) {
+        console.log(erro);
+    }
 }
 
 //POST
-function createWoman(request, response) {
-    const newWoman = {
-        id: uuidv4(),
+async function createWoman(request, response) {
+    const newWoman = new Woman({        
         name: request.body.name,
         image: request.body.image,
-        tinybio: request.body.tinybio
-    }
+        tinybio: request.body.tinybio,
+        quote: request.body.quote
+    });
 
-    womans.push(newWoman);
-    response.json(womans);
+    try {
+        const createdWoman = await newWoman.save();
+        response.status(201).json(createdWoman);
+    } catch(erro) {
+        console.log(erro)
+    }
 }
 
 //PATCH
-function correctWoman(request, response) {
-    function findWoman(woman) {
-        if (woman.id === request.params.id) {
-            return woman;
+async function correctWoman(request, response) { 
+
+    try {
+        const womanFound = await Woman.findById(request.params.id);
+
+        if (request.body.name) {
+            womanFound.name = request.body.name;
         }
-    }
+        if (request.body.image) {
+            womanFound.image = request.body.image;
+        }
+        if (request.body.tinybio) {
+            womanFound.tinybio = request.body.tinybio;
+        }
+        if (request.body.quote) {
+            womanFound.quote = request.body.quote;
+        }
 
-    const womanFound = womans.find(findWoman);
+        const updateWomanInDb = await womanFound.save();
 
-    if (request.body.name) {
-        womanFound.name = request.body.name;
+        response.json(updateWomanInDb);
+    
+    } catch(erro){
+        console.log(erro)
     }
-    if (request.body.image) {
-        womanFound.image = request.body.image;
-    }
-    if (request.body.tinybio) {
-        womanFound.tinybio = request.body.tinybio;
-    }
-
-    response.json(womans);
-
+    
 }
 
 //DELETE
